2f104cbb2a29f63251cb5b6d10c18881d15ef7d7
Tristan Bunyan <tristan.bunyan@justis.com>
2018-07-03 10:34:06 +0100

    end of module 5 I think

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class CoursesPage extends Component {
+  render() {
+    return (
+      <div>
+          <h1>Courses</h1>
+      </div>
+    );
+  }
+}
+
+
+export default CoursesPage;
